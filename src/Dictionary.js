a513866b06de0dc4e76a90cfa6bf21cd5ce2da3f
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 12:52:37 +0200

    dictionary
    added api call using axios and apil function with keyword

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+import { c } from "tar";
 
 export default function Dictionary(){
     let[keyword, setKeyword] = useState("");
 
 
+    function handleResponse(response){
+        console.log(response.data[0]);
+    }
+
 function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl =`https://https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
 }
+
+
 
 function HandleKeywordChange(event){
     setKeyword(event.target.value);
