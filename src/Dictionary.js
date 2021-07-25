b2417c25f14a2a9474a44f82f91e570093204ce8
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 12:54:22 +0200

    amended dictionary api intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
-import { c } from "tar";
+
 
 export default function Dictionary(){
     let[keyword, setKeyword] = useState("");
