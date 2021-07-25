69a374d9afdcccf3bf32fbc2c6f9b913c847a860
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 11:17:11 +0200

    added image
    initial start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import logo from "./logo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header"></header>
-    HEllO
+    <header className="App-header">
+    <img src={logo} className="App-logo img-fluid" alt="logo"/>
+</header>
+    <h1>Dictionary App</h1>
     </div>
   );
 }
