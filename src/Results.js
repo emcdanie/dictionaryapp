d678fad0ea009eed339dac1cde7d8d912b9014c0
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 16:31:34 +0200

    Created Phonetic Component

--- src/Results.js
+++ src/Results.js
@@ -1,22 +1,29 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 
-export default function Results(props){
-    if (props.results){
+export default function Results(props) {
+  if (props.results) {
     return (
-    <div className="Results"> 
+      <div className="Results">
         <h2>{props.results.word}</h2>
-    {props.results.meanings.map(function(meaning, index){
-        return (
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
             <div key={index}>
-            <Meaning meaning={meaning}/>
+              <Phonetic phonetic={phonetic} />
             </div>
-        );
-    })}
-    </div>
+          );
+        })}
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
     );
-
-     } else {
-        return null;
-    }
+  } else {
+    return null;
+  }
 }
